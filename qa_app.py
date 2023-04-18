5bb72136ecf1f21359af8e77fd14d43967a3b649
Your Name <you@example.com>
2023-04-18 13:04:06 +0000

    Initial commit

---
+++ qa_app.py
@@ -0,0 +1,228 @@
+import os
+import json
+import time
+import pypdf
+import random
+import itertools
+import text_utils
+import pandas as pd
+import altair as alt
+import streamlit as st
+from io import StringIO
+from langchain.document_loaders import TextLoader
+from pathlib import Path
+from langchain.vectorstores import FAISS
+from langchain.chains import RetrievalQA
+from langchain.chat_models import ChatOpenAI
+from langchain.retrievers import SVMRetriever
+from langchain.chains import QAGenerationChain
+from langchain.retrievers import TFIDFRetriever
+from langchain.evaluation.qa import QAEvalChain
+from langchain.embeddings import HuggingFaceEmbeddings
+from langchain.embeddings.openai import OpenAIEmbeddings
+from langchain.retrievers.llama_index import LlamaIndexRetriever
+from text_utils import GRADE_DOCS_PROMPT, GRADE_ANSWER_PROMPT, GRADE_DOCS_PROMPT_FAST, GRADE_ANSWER_PROMPT_FAST
+from langchain.text_splitter import RecursiveCharacterTextSplitter, CharacterTextSplitter
+
+from langchain.callbacks.streaming_stdout import StreamingStdOutCallbackHandler
+from langchain.callbacks.base import CallbackManager
+
+st.set_page_config(page_title="RetrievalQA App")
+
+# os.environ["OPENAI_API_KEY"] = "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"
+
+@st.cache_data
+def load_docs(files):
+    st.info("`Reading doc ...`")
+    all_text = ""
+    for file_path in files:
+        file_extension = os.path.splitext(file_path.name)[1]
+        if file_extension == ".pdf":
+            pdf_reader = pypdf.PdfReader(file_path)
+            text = ""
+            for page in pdf_reader.pages:
+                text += page.extract_text()
+            all_text += text
+        elif file_extension == ".txt":
+            stringio = StringIO(file_path.getvalue().decode("utf-8"))
+            text = stringio.read()
+            all_text += text
+        else:
+            st.warning('Please provide txt or pdf.', icon="⚠️")
+    return all_text
+
+@st.cache_resource
+def create_retriever(_embeddings, splits, retriever_type):
+    if retriever_type == "SIMILARITY SEARCH":
+        vectorstore = FAISS.from_texts(splits, _embeddings)
+        retriever = vectorstore.as_retriever(k=5)
+    elif retriever_type == "SUPPORT VECTOR MACHINES":
+        retriever = SVMRetriever.from_texts(splits, _embeddings)
+
+    return retriever
+
+@st.cache_resource
+def split_texts(text, chunk_size, overlap, split_method):
+
+    # Split texts
+    # IN: text, chunk size, overlap, split_method
+    # OUT: list of str splits
+
+    st.info("`Splitting doc ...`")
+
+    split_method = "RecursiveTextSplitter"
+    text_splitter = RecursiveCharacterTextSplitter(
+        chunk_size=chunk_size, chunk_overlap=overlap)
+
+    splits = text_splitter.split_text(text)
+
+    return splits
+
+@st.cache_data
+def generate_eval(text, N, chunk):
+
+    # Generate N questions from context of chunk chars
+    # IN: text, N questions, chunk size to draw question from in the doc
+    # OUT: eval set as JSON list
+
+    st.info("`Generating sample questions ...`")
+    n = len(text)
+    starting_indices = [random.randint(0, n-chunk) for _ in range(N)]
+    sub_sequences = [text[i:i+chunk] for i in starting_indices]
+    chain = QAGenerationChain.from_llm(ChatOpenAI(temperature=0))
+    eval_set = []
+    for i, b in enumerate(sub_sequences):
+        try:
+            qa = chain.run(b)
+            eval_set.append(qa)
+        except:
+            st.warning('Error generating question %s.' % str(i+1), icon="⚠️")
+    eval_set_full = list(itertools.chain.from_iterable(eval_set))
+    return eval_set_full
+
+
+# ...
+
+def main():
+    # Add custom CSS
+    st.markdown(
+        """
+        <style>
+            .css-card {
+                border-radius: 0px;
+                padding: 30px 10px 10px 10px;
+                background-color: #f8f9fa;
+                box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);
+                margin-bottom: 10px;
+                font-family: "IBM Plex Sans", sans-serif;
+            }
+            
+            .card-tag {
+                border-radius: 0px;
+                padding: 1px 5px 1px 5px;
+                margin-bottom: 10px;
+                position: absolute;
+                left: 0px;
+                top: 0px;
+                font-size: 0.6rem;
+                font-family: "IBM Plex Sans", sans-serif;
+                color: white;
+                background-color: green;
+                }
+                
+                
+                
+                
+        
+        </style>
+        """,
+        unsafe_allow_html=True,
+    )
+
+    st.title("RetrievalQA App")
+
+    st.sidebar.title("Menu")
+    retriever_type = st.sidebar.selectbox(
+        "Choose Retriever", ["SIMILARITY SEARCH", "SUPPORT VECTOR MACHINES"])
+
+    # Use RecursiveCharacterTextSplitter as the default and only text splitter
+    splitter_type = "RecursiveCharacterTextSplitter"
+
+    if 'openai_api_key' not in st.session_state:
+        openai_api_key = st.sidebar.text_input(
+            "Enter your OpenAI API key:", value="", placeholder="sk-")
+        if openai_api_key:
+            st.session_state.openai_api_key = openai_api_key
+            os.environ["OPENAI_API_KEY"] = openai_api_key
+        else:
+            st.sidebar.warning("Please enter your OpenAI API key.")
+            return
+    else:
+        os.environ["OPENAI_API_KEY"] = st.session_state.openai_api_key
+
+    uploaded_files = st.file_uploader("Upload a PDF or TXT Document", type=[
+                                      "pdf", "txt"], accept_multiple_files=True)
+
+    if uploaded_files:
+        # Check if last_uploaded_files is not in session_state or if uploaded_files are different from last_uploaded_files
+        if 'last_uploaded_files' not in st.session_state or st.session_state.last_uploaded_files != uploaded_files:
+            st.session_state.last_uploaded_files = uploaded_files
+            if 'eval_set' in st.session_state:
+                del st.session_state['eval_set']
+
+        # Load and process the uploaded PDF or TXT files
+        loaded_text = load_docs(uploaded_files)
+        st.write("Documents uploaded and processed.")
+
+        # Split the document into chunks
+        splits = split_texts(loaded_text, chunk_size=1000,
+                             overlap=0, split_method=splitter_type)
+
+        # Display the number of text chunks
+        num_chunks = len(splits)
+        st.write(f"Number of text chunks: {num_chunks}")
+
+        # Embed using OpenAI embeddings
+        embeddings = OpenAIEmbeddings()
+        retriever = create_retriever(embeddings, splits, retriever_type)
+
+        # Initialize the RetrievalQA chain with streaming output
+        callback_handler = StreamingStdOutCallbackHandler()
+        callback_manager = CallbackManager([callback_handler])
+
+        chat_openai = ChatOpenAI(
+            streaming=True, callback_manager=callback_manager, verbose=True, temperature=0)
+        qa = RetrievalQA.from_chain_type(llm=chat_openai, retriever=retriever, chain_type="stuff", verbose=True)
+
+        # Check if there are no generated question-answer pairs in the session state
+        if 'eval_set' not in st.session_state:
+            # Use the generate_eval function to generate question-answer pairs
+            num_eval_questions = 10  # Number of question-answer pairs to generate
+            st.session_state.eval_set = generate_eval(
+                loaded_text, num_eval_questions, 3000)
+
+       # Display the question-answer pairs in the sidebar with smaller text
+        for i, qa_pair in enumerate(st.session_state.eval_set):
+            st.sidebar.markdown(
+                f"""
+                <div class="css-card">
+                <span class="card-tag">Question {i + 1}</span>
+                    <p style="font-size: 12px;">{qa_pair['question']}</p>
+                    <p style="font-size: 12px;">{qa_pair['answer']}</p>
+                </div>
+                """,
+                unsafe_allow_html=True,
+            )
+            # <h4 style="font-size: 14px;">Question {i + 1}:</h4>
+            # <h4 style="font-size: 14px;">Answer {i + 1}:</h4>
+        st.write("Ready to answer questions.")
+
+        # Question and answering
+        user_question = st.text_input("Enter your question:")
+        if user_question:
+            answer = qa.run(user_question)
+            st.write("Answer:", answer)
+
+
+if __name__ == "__main__":
+    main()
